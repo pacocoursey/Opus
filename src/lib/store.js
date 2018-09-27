f3b69f956f6e248212a7ff3d4c3bee7b27f739f5
pacocoursey <pvco.coursey@gmail.com>
2018-09-27 15:51:23 -0600

    improve store singleton pattern

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,18 +1,26 @@
 const settings = require('electron-settings');
 
-let path;
+const Store = {
+  init: (p) => {
+    this.path = p;
+  },
 
-module.exports = {
-  init(str) {
-    path = str;
+  get: (key) => {
+    if (!this.path) throw new Error('No store path defined.');
+    return settings.get(`windows.${this.path}.${key}`);
   },
-  set(key, value) {
-    return settings.set(`windows.${path}.${key}`, value);
+
+  set: (key, value) => {
+    if (!this.path) throw new Error('No store path defined.');
+    return settings.set(`windows.${this.path}.${key}`, value);
   },
-  has(key) {
-    return settings.has(`windows.${path}.${key}`);
-  },
-  get(key) {
-    return settings.get(`windows.${path}.${key}`);
+
+  has: (key) => {
+    if (!this.path) throw new Error('No store path defined.');
+    return settings.has(`windows.${this.path}.${key}`);
   },
 };
+
+Object.freeze(Store);
+
+module.exports = Store;
