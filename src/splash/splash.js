c82c3c9db9537a1097df113f6c9170207f8d2eb2
pacocoursey <pvco.coursey@gmail.com>
2018-10-02 20:38:19 -0600

    overflow when there are lots of recent projects, add ability to remove a recent project via right click

--- src/splash/splash.js
+++ src/splash/splash.js
@@ -1,28 +1,39 @@
 const path = require('path');
 const ipc = require('electron-better-ipc');
 const settings = require('electron-settings');
-const { shell } = require('electron');
+const { shell, remote } = require('electron');
 
+let p;
+let menu;
+const { Menu } = remote;
 const aside = document.querySelector('aside');
 const footer = document.querySelector('footer');
 
-function listItem(p) {
+/**
+ * Create a new list-item div from the given path and return it.
+ */
+
+function listItem(dataPath) {
   const itemDiv = document.createElement('div');
   const nameDiv = document.createElement('div');
   const pathDiv = document.createElement('div');
 
   itemDiv.classList.add('list-item');
-  itemDiv.setAttribute('data-path', p);
+  itemDiv.setAttribute('data-path', dataPath);
   nameDiv.classList.add('name');
-  nameDiv.innerText = path.basename(p);
+  nameDiv.innerText = path.basename(dataPath);
   pathDiv.classList.add('path');
-  pathDiv.innerText = path.dirname(p);
+  pathDiv.innerText = path.dirname(dataPath);
 
   itemDiv.appendChild(nameDiv);
   itemDiv.appendChild(pathDiv);
 
   return itemDiv;
 }
+
+/**
+ * Custom comparison function to sort windows by time last opened.
+ */
 
 function compare(a, b) {
   if (a.opened > b.opened) {
@@ -36,40 +47,19 @@ function compare(a, b) {
   return 0;
 }
 
-if (settings.has('windows')) {
-  const windows = Object.values(settings.get('windows'));
-  windows.sort(compare);
+/**
+ * Remove active class from whatever is currently active.
+ */
 
-  if (windows.length === 0) {
-    aside.innerHTML = '<div class="no-recent">No Recent Folders</div>';
-  } else {
-    const list = document.createElement('div');
-    list.classList.add('list');
-
-    windows.forEach((win) => {
-      const item = listItem(win.path);
-      list.appendChild(item);
-    });
-
-    list.children[0].classList.add('active');
-
-    aside.appendChild(list);
-  }
-} else {
-  aside.innerHTML = '<div class="no-recent">No Recent Folders</div>';
+function removeActive() {
+  document.querySelector('.active').classList.remove('active');
 }
 
-document.querySelectorAll('.link').forEach((link) => {
-  link.addEventListener('click', () => {
-    shell.openExternal('https://github.com/pacocoursey/Opus/');
-  });
-});
+/**
+ * Adds active class to the appropriate div.
+ */
 
-const removeActive = () => {
-  document.querySelector('.active').classList.remove('active');
-};
-
-const get = (increment) => {
+function get(increment) {
   const currentlyActive = document.querySelector('.active');
   const list = document.querySelector('.list');
   let i = 0;
@@ -85,47 +75,146 @@ const get = (increment) => {
   }
 
   return list.children[i + increment];
-};
+}
 
-const openPath = async (p) => {
-  await ipc.callMain('openProject', p);
-};
+/**
+ * Calls main process to open the selected project.
+ */
 
-document.addEventListener('keydown', (e) => {
-  if (e.keyCode === 38) {
-    const el = get(-1);
-    removeActive();
-    el.classList.add('active');
-  } else if (e.keyCode === 40) {
-    const el = get(1);
-    removeActive();
-    el.classList.add('active');
-  } else if (e.keyCode === 13) {
-    const active = document.querySelector('.active');
-    openPath(active.getAttribute('data-path'));
-  }
-});
+async function openPath(dataPath) {
+  await ipc.callMain('openProject', dataPath);
+}
 
-footer.addEventListener('click', async () => {
-  await ipc.callMain('openProject');
-});
+/**
+ * Creates and appends a new list-item div for each window
+ * stored in the settings.
+ */
 
-document.querySelector('.close').addEventListener('click', async () => {
-  ipc.callMain('closeSplashWindow');
-});
+function populateSidebar() {
+  // Reset the content.
+  aside.innerHTML = '';
+  if (settings.has('windows')) {
+    const windows = Object.values(settings.get('windows'));
+    windows.sort(compare);
 
-const listItems = document.querySelectorAll('.list-item');
-listItems.forEach((item) => {
-  item.addEventListener('click', () => {
-    if (item.classList.contains('active')) {
-      return;
+    if (windows.length === 0) {
+      aside.innerHTML = '<div class="no-recent">No Recent Folders</div>';
+    } else {
+      const list = document.createElement('div');
+      list.classList.add('list');
+
+      windows.forEach((win) => {
+        const item = listItem(win.path);
+        list.appendChild(item);
+      });
+
+      list.children[0].classList.add('active');
+
+      aside.appendChild(list);
     }
+  } else {
+    aside.innerHTML = '<div class="no-recent">No Recent Folders</div>';
+  }
+}
 
-    removeActive();
-    item.classList.add('active');
+/**
+ * Setup all the document listeners.
+ */
+
+function initListeners() {
+  // Send message to main process to close the splash window.
+  document.querySelector('.close').addEventListener('click', async () => {
+    ipc.callMain('closeSplashWindow');
   });
 
-  item.addEventListener('dblclick', () => {
-    openPath(item.getAttribute('data-path'));
+  // Send message to main process to prompt for a folder.
+  footer.addEventListener('click', async () => {
+    await ipc.callMain('openProject');
   });
-});
+
+  // Open GitHub page when each link is clicked.
+  document.querySelectorAll('.link').forEach((link) => {
+    link.addEventListener('click', () => {
+      shell.openExternal('https://github.com/pacocoursey/Opus/');
+    });
+  });
+
+
+  // Add active class to the relevant list-item div.
+  document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 38) {
+      const el = get(-1);
+      removeActive();
+      el.classList.add('active');
+    } else if (e.keyCode === 40) {
+      const el = get(1);
+      removeActive();
+      el.classList.add('active');
+    } else if (e.keyCode === 13) {
+      const active = document.querySelector('.active');
+      openPath(active.getAttribute('data-path'));
+    }
+  });
+
+  // Add active class to clicked item
+  // Add active class to right-clicked item
+  // Open the window for double-clicked items
+  document.querySelectorAll('.list-item').forEach((item) => {
+    item.addEventListener('click', () => {
+      if (item.classList.contains('active')) {
+        return;
+      }
+
+      removeActive();
+      item.classList.add('active');
+    });
+
+    item.addEventListener('contextmenu', () => {
+      if (item.classList.contains('active')) {
+        return;
+      }
+
+      removeActive();
+      item.classList.add('active');
+    });
+
+    item.addEventListener('dblclick', () => {
+      openPath(item.getAttribute('data-path'));
+    });
+  });
+
+  // Show a context menu when a list-item div is clicked.
+  window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+
+    const element = e.target.closest('.list-item');
+
+    if (element) {
+      p = element.getAttribute('data-path');
+      menu.popup(remote.getCurrentWindow());
+    }
+  });
+}
+
+/**
+ * Remove a project with path `p` from the settings object.
+ */
+
+function removeProject(dataPath) {
+  if (settings.has(`windows.${dataPath}`)) {
+    settings.delete(`windows.${dataPath}`);
+    populateSidebar();
+    initListeners();
+  }
+}
+
+
+menu = Menu.buildFromTemplate([
+  {
+    label: 'Remove',
+    click: () => { removeProject(p); },
+  },
+]);
+
+populateSidebar();
+initListeners();
